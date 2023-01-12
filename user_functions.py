c1caaeba57227aa5003474ec6b1b6c304b1101cc
RedaChaibi <redachaibi.08@gmail.com>
2023-01-12 17:04:31 +0100

    New function

--- user_functions.py
+++ user_functions.py
@@ -13,4 +13,14 @@ def get_user_name_from_input():
         print ('User name is not valid')
     else : 
         return username
+
+
+def get_password_from_input():
+    import string
+    """ Password needs to be at least 8 characters long with at least one number, one special character and one letter. """
+    password = input("Create your password: ")
+    if ([i for i in range(10)] not in password or list(string.punctuation) not in password or list(string.ascii_letters) not in password or len(password) < 8) : 
+        print("Password is not valid")
+    else : 
+        return password 
     
