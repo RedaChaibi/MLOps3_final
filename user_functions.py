3281e344cc6dfb958a211cc153df9ecc73824478
RedaChaibi <redachaibi.08@gmail.com>
2023-01-12 15:57:51 +0100

    First Commit

---
+++ user_functions.py
@@ -0,0 +1,7 @@
+def get_email_from_input():
+    """ Contains '@' and '.' """
+    email = input("Write down your email: ")
+    if ("@" not in email or "." not in email):
+        print('Email is not valid.')
+    else:
+        return email
